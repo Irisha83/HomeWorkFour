2c87eefcdd6c9eb364e0e8e202c17ac8e50e9e2f
Irina <arina_kovrov@mail.ru>
2022-11-29 17:02:54 +0300

    первый коммит

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// 1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// int Exponent(int numberA, int numberB){
+//   int result = 1;
+//   for(int i=1; i <= numberB; i++)
+//   {
+//     result = result * numberA;
+//   }
+//         return result;
+// }
+
+//   Console.Write("Введите число A: ");
+//   int numberA = Convert.ToInt32(Console.ReadLine());
+//   Console.Write("Введите число B: ");
+//   int numberB = Convert.ToInt32(Console.ReadLine());
+
+//   int exponentiation = Exponent(numberA, numberB);
+//   Console.WriteLine("Ответ: " + exponentiation);
+
+// 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN)
+  {
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+//  3. Напишите программу, которая генерирует массив из 8 элементов - случайных чисел (от 0 до 20) и выводит их на экран.
+
+// int [] array = new int [8];
+// for (int i = 0; i < 8; i++)
+// {
+//     int randomNumber = new Random().Next(0,21);
+//     array [i] = randomNumber;
+//     Console.WriteLine(array[i]);
+// }
